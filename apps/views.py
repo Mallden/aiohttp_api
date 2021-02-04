041af10545270d23c32d44327a2dd84fc0e1e53c
Mallden <doc34ru@gmail.com>
2021-02-04 11:45:10 +0300

    refresh views info_clicks

--- apps/views.py
+++ apps/views.py
@@ -5,6 +5,7 @@ from aiohttp import web
 from aiochorm import utils
 from apps.models.models import ClickHouseCompanyLog
 from apps.database import DB_CONNECT
+from apps.settings import DICT_KEY, TO_DATE
 
 QUERYSET = ClickHouseCompanyLog.objects_in_async(DB_CONNECT)
 
@@ -13,7 +14,14 @@ async def info_clicks(request):
         return web.json_response({'error': 'Method Not Allowed', 'status code': 405})
     print(dict(request.query))
 
-    queryset = QUERYSET.filter(event=2, ts__gte=datetime.datetime.now() - datetime.timedelta(days=2))
+    filter_queryset = {
+        DICT_KEY[key]
+        if key in DICT_KEY.keys()
+        else key: datetime.datetime.strptime(request.query[key], '%Y.%m.%d')
+        if key in TO_DATE else request.query[key]
+        for key in request.query
+    }
+    queryset = QUERYSET.filter(**filter_queryset)
 
     events = await queryset.execute()
     return web.json_response(text=json.dumps(events, cls=utils.JSONEncoder))
