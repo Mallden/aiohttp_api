48aa98a05cdf093b2c7c0d587ae81608d4572167
Mallden <doc34ru@gmail.com>
2021-02-08 18:01:28 +0300

    refresh views

--- api/views.py
+++ api/views.py
@@ -8,23 +8,27 @@ from database import DB_CONNECT
 from settings import DICT_KEY, TO_DATE
 
 QUERYSET = ClickHouseCompanyLog.objects_in_async(DB_CONNECT)
-
+FIELDS_MODEL = list(ClickHouseCompanyLog.fields().keys())
 
 class InfoClickView(web.View):
 
     async def get(self):
         if not self.request.query:
             return web.json_response(status=400)
+
+        data = dict(self.request.query)
+        fields = data.pop('fields').split(',') if data.get('fields') else None
+
         filter_queryset = {
             DICT_KEY[key]
             if key in DICT_KEY.keys()
-            else key: datetime.datetime.strptime(self.request.query[key], '%Y.%m.%d')
-            if key in TO_DATE else self.request.query[key]
-            for key in self.request.query
+            else key: datetime.datetime.strptime(data[key], '%Y.%m.%d')
+            if key in TO_DATE else data[key]
+            for key in data
         }
         filter_queryset['company_id'] = int(self.request.match_info.get('company'))
-        queryset = QUERYSET.filter(**filter_queryset)
 
+        queryset = QUERYSET.filter(**filter_queryset).only(*fields) if fields else QUERYSET.filter(**filter_queryset)
         events = await queryset.execute()
 
         if not events:
